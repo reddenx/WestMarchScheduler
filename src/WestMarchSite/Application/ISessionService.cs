4069f596747ec6a52a7378e7d2cec7bcd16e2017
sm_d3po <seanmarek@gmail.com>
2021-11-29 23:58:27 -0800

    initial api contract developed

---
+++ src/WestMarchSite/Application/ISessionService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WestMarchSite.Application
+{
+    public interface ISessionService
+    {
+        CreateSessionResultDto StartSession(string leadKey, CreateSessionDto createDto);
+        ApproveSessionResultDto DmApproveSession(string dmKey, ApproveSessionDto approvalDto);
+        LeadScheduleResultDto LeadNarrowsSchedule(string leadKey, LeadScheduleDto leadScheduleDto);
+        bool PlayerJoinSession(string playerKey, PlayerJoinDto playerDto);
+        bool DmFinalizes(string dmKey);
+
+        SessionDto GetPlayerSession(string playerKey);
+        SessionDto GetDmSession(string dmKey);
+        SessionDto GetLeadSession(string leadKey);
+    }
+
+    public class CreateSessionDto
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class CreateSessionResultDto
+    {
+        public string DmKey { get; set; }
+        public string LeadKey { get; set; }
+        public string PlayerKey { get; set; }
+    }
+
+    public class ApproveSessionDto
+    {
+        public string Name { get; set; }
+        public SessionScheduleDateDto[] Schedule { get; set; }
+    }
+
+    public class ApproveSessionResultDto
+    {
+        public string DmKey { get; set; }
+    }
+
+    public class LeadScheduleDto
+    {
+        public SessionScheduleDateDto[] Schedule { get; set; }
+    }
+
+    public class LeadScheduleResultDto
+    {
+        public string LeadKey { get; set; }
+        public string DmKey { get; set; }
+        public string PlayerKey { get; set; }
+    }
+
+    public class PlayerJoinDto
+    {
+        public string Name { get; set; }
+        public SessionScheduleDateDto[] Schedule { get; set; }
+    }
+
+    public class SessionDto
+    {
+        //probably best to keep these only on the entity
+        //public string PlayerKey { get; set; }
+        //public string DmKey { get; set; }
+        //public string LeadKey { get; set; }
+
+        public SessionStatusDto Status { get; set; }
+
+        public HostDto Host { get; set; }
+        public LeadDto Lead { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PostDate { get; set; }
+
+        public PlayerDto[] Players { get; set; }
+
+        public class HostDto
+        {
+            public string Name { get; set; }
+            public SessionScheduleDateDto[] Schedule { get; set; }
+        }
+        public class LeadDto
+        {
+            public string Name { get; set; }
+            public SessionScheduleDateDto[] Schedule { get; set; }
+        }
+        public class PlayerDto
+        {
+            public string Name { get; set; }
+            public SessionScheduleDateDto[] Schedule { get; set; }
+        }
+
+        public enum SessionStatusDto
+        {
+            /// <summary>
+            /// idea has been posted but unapproved by DM
+            /// </summary>
+            Initiated,
+            /// <summary>
+            /// DM has approved and set their schedule, waiting for leader schedule selection
+            /// </summary>
+            Approved,
+            /// <summary>
+            /// Session is open for player joining
+            /// </summary>
+            Open,
+            /// <summary>
+            /// Session has been closed to joining
+            /// </summary>
+            Finalized
+        }
+    }
+
+    public class SessionScheduleDateDto
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+
+}
---
+++ src/WestMarchSite/Controllers/SessionController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WestMarchSite.Application;
+
+namespace WestMarchSite.Controllers
+{
+    [Route("api/sessions")]
+    public class SessionController : Controller
+    {
+        private readonly ISessionService _sessionService;
+
+        public SessionController(ISessionService sessionService)
+        {
+            _sessionService = sessionService;
+        }
+
+        [HttpPost("{key}")]
+        [ProducesResponseType(200, Type = typeof(CreateSessionResultDto))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult CreateSession([FromRoute]string key, [FromBody]CreateSessionDto create)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{key}/approve")]
+        [ProducesResponseType(200, Type = typeof(ApproveSessionResultDto))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult ApproveSession([FromRoute]string key, [FromBody]ApproveSessionDto approval)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{key}/schedule")]
+        [ProducesResponseType(200, Type = typeof(LeadScheduleResultDto))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult LeadSchedule([FromRoute]string key, [FromBody]LeadScheduleDto lead)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{key}/join")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult PlayerJoin([FromRoute]string key, [FromBody]PlayerJoinDto join)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{key}/finalize")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult Finalize([FromRoute]string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
