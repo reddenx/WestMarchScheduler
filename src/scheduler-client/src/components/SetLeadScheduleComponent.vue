ad8fcf7fb73a1184d45d0c35a5ed5aed9d8e93b4
Sean <reddenx@users.noreply.github.com>
2022-02-22 00:12:37 -0800

    Blind coding on a phone

--- src/scheduler-client/src/components/SetLeadScheduleComponent.vue
+++ src/scheduler-client/src/components/SetLeadScheduleComponent.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-      Set Lead Schedule Component
+      Please enter your name and choice of available schedule.
+<input type="text" v-model="name" class="form-control"/>
+<ScheduleComponent
+/>
+<button type="button" class="btn btn-primary mt-2" @click="submitPressed">
+Submit</button>
+
+
   </div>
 </template>
 
 <script>
+import ScheduleComponent from "./ScheduleComponent"
+
 export default {
+components:[ScheduleComponent]
+data:()=>({
+}),
+methods:{
+submitPressed() {
+alert('test');
+},
+},
 
 }
 </script>
